3399c4d8351f1db3092a9d988c8444e42469418a
Halimatou <halimatou.cisse@gmail.com>
2023-10-03 15:50:57 +0200

    test netlify

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,7 +9,8 @@ const router = createRouter({
     {
       path: '/prestations',
       name: 'Prestations',
-      component: () => import('@/components/PrestationsPage.vue')
+      component: () => import('@/components/PrestationsPage.vue'),
+      
     },
     {
       path: '/contact',
